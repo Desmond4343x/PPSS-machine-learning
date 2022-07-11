92d1f0da7ac8dcc8105f1410b278b8e43502035a
ponpan <ponpan@kth.se>
2022-07-11 19:08:15 +0200

    did deep autoencoder

--- first_autoencoder.py
+++ first_autoencoder.py
@@ -1,72 +1,71 @@
-import tensorflow
-import keras
-from keras import layers
 from keras.datasets import mnist
+from keras.layers import Input, Dense
+from keras.models import Model
 import numpy as np
-# Use Matplotlib (don't ask)
+import pandas as pd
 import matplotlib.pyplot as plt
 
+#Loading training data
+(X_train, _), (X_test, _) = mnist.load_data()
+X_train = X_train.astype('float32')/255
+X_test = X_test.astype('float32')/255
+X_train = X_train.reshape(len(X_train), np.prod(X_train.shape[1:]))
+X_test = X_test.reshape(len(X_test), np.prod(X_test.shape[1:]))
+print(X_train.shape)
+print(X_test.shape)
 
+#Neural net
+input_img = Input(shape=(784,))
 
-# This is the size of our encoded representations
-encoding_dim = 32  # 32 floats -> compression of factor 24.5, assuming the input is 784 floats
+encoded = Dense(units=128, activation='relu')(input_img)
+encoded = Dense(units=64, activation='relu')(encoded)
+encoded = Dense(units=32, activation='relu')(encoded)
+encoded = Dense(units=16, activation='relu')(encoded)
+encoded = Dense(units=8, activation='relu')(encoded)
+encoded = Dense(units=4, activation='relu')(encoded)
 
-# This is our input image
-input_img = keras.Input(shape=(784,))
-# "encoded" is the encoded representation of the input
-encoded = layers.Dense(encoding_dim, activation='relu')(input_img)
-# "decoded" is the lossy reconstruction of the input
-decoded = layers.Dense(784, activation='sigmoid')(encoded)
+decoded = Dense(units=8, activation='relu')(encoded)
+decoded = Dense(units=16, activation='relu')(encoded)
+decoded = Dense(units=32, activation='relu')(encoded)
+decoded = Dense(units=64, activation='relu')(encoded)
+decoded = Dense(units=128, activation='relu')(decoded)
+decoded = Dense(units=784, activation='sigmoid')(decoded)
 
-# This model maps an input to its reconstruction
-autoencoder = keras.Model(input_img, decoded)
+autoencoder=Model(input_img, decoded)
 
-# This model maps an input to its encoded representation
-encoder = keras.Model(input_img, encoded)
+encoder = Model(input_img, encoded)
 
-# This is our encoded (32-dimensional) input
-encoded_input = keras.Input(shape=(encoding_dim,))
-# Retrieve the last layer of the autoencoder model
-decoder_layer = autoencoder.layers[-1]
-# Create the decoder model
-decoder = keras.Model(encoded_input, decoder_layer(encoded_input))
+autoencoder.summary()
 
-autoencoder.compile(optimizer='adam', loss='binary_crossentropy')
+encoder.summary()
 
-(x_train, _), (x_test, _) = mnist.load_data()
+autoencoder.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 
-x_train = x_train.astype('float32') / 255.
-x_test = x_test.astype('float32') / 255.
-x_train = x_train.reshape((len(x_train), np.prod(x_train.shape[1:])))
-x_test = x_test.reshape((len(x_test), np.prod(x_test.shape[1:])))
-print(x_train.shape)
-print(x_test.shape)
-
-autoencoder.fit(x_train, x_train,
-                epochs=50,
+autoencoder.fit(X_train, X_train,
+                epochs=1000,
                 batch_size=256,
                 shuffle=True,
-                validation_data=(x_test, x_test))
-# Encode and decode some digits
-# Note that we take them from the *test* set
-encoded_imgs = encoder.predict(x_test)
-decoded_imgs = decoder.predict(encoded_imgs)
+                validation_data=(X_test, X_test))
+
+encoded_imgs = encoder.predict(X_test)
+predicted = autoencoder.predict(X_test)
+
+plt.figure(figsize=(40, 4))
+for i in range(10):
+	# display original images
+	ax = plt.subplot(3, 20, i + 1)
+	plt.imshow(X_test[i].reshape(28, 28))
+	plt.gray()
+	ax.get_xaxis().set_visible(False)
+	ax.get_yaxis().set_visible(False)
 
 
-n = 10  # How many digits we will display
-plt.figure(figsize=(20, 4))
-for i in range(n):
-    # Display original
-    ax = plt.subplot(2, n, i + 1)
-    plt.imshow(x_test[i].reshape(28, 28))
-    plt.gray()
-    ax.get_xaxis().set_visible(False)
-    ax.get_yaxis().set_visible(False)
 
-    # Display reconstruction
-    ax = plt.subplot(2, n, i + 1 + n)
-    plt.imshow(decoded_imgs[i].reshape(28, 28))
-    plt.gray()
-    ax.get_xaxis().set_visible(False)
-    ax.get_yaxis().set_visible(False)
+	# display reconstructed images
+	ax = plt.subplot(3, 20, 2 * 20 + i + 1)
+	plt.imshow(predicted[i].reshape(28, 28))
+	plt.gray()
+	ax.get_xaxis().set_visible(False)
+	ax.get_yaxis().set_visible(False)
+
 plt.show()
