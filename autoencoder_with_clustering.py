95dc20b8b2a2ba458e5c444359f289214a91df2d
ponpan <ponpan@kth.se>
2022-07-12 16:37:48 +0200

    second cluster images

--- autoencoder_with_clustering.py
+++ autoencoder_with_clustering.py
@@ -32,20 +32,20 @@ print(X_test.shape)
 
 #Neural net
 input_img = Input(shape=(784,))
-#x²
+#log
 
-encoded = Dense(units=441, activation='relu')(input_img)
-encoded = Dense(units=256, activation='relu')(encoded)
-encoded = Dense(units=100, activation='relu')(encoded)
-encoded = Dense(units=25, activation='relu')(encoded)
-encoded = Dense(units=16, activation='relu')(encoded)
+encoded = Dense(units=628, activation='relu')(input_img)
+encoded = Dense(units=180, activation='relu')(encoded)
+encoded = Dense(units=66, activation='relu')(encoded)
+encoded = Dense(units=19, activation='relu')(encoded)
+encoded = Dense(units=8, activation='relu')(encoded)
 encoded = Dense(units=4, activation='linear')(encoded)
 
-decoded = Dense(units=16, activation='relu')(encoded)
-decoded = Dense(units=25, activation='relu')(decoded)
-decoded = Dense(units=100, activation='relu')(decoded)
-decoded = Dense(units=256, activation='relu')(decoded)
-decoded = Dense(units=441, activation='relu')(decoded)
+decoded = Dense(units=8, activation='relu')(encoded)
+decoded = Dense(units=19, activation='relu')(decoded)
+decoded = Dense(units=66, activation='relu')(decoded)
+decoded = Dense(units=180, activation='relu')(decoded)
+decoded = Dense(units=628, activation='relu')(decoded)
 decoded = Dense(units=784, activation='sigmoid')(decoded)
 
 autoencoder = Model(input_img, decoded)
