6634d053e02bc03ae8bc99f3dcf11ee905cb5d00
ponpan <ponpan@kth.se>
2022-07-21 14:32:58 +0200

    physics approach filter and sorter 90% finished

--- physics_approach.py
+++ physics_approach.py
@@ -11,6 +11,52 @@ def main
 	load data with eventual filter and call every function"""
 
 
+def x_values(training_data):
+	outer_array = []
+	picture_index = 0
+
+	while picture_index <= (training_data.shape[0]-1):
+		sum = 0
+		for y_index in range(training_data.shape[1]):
+			for x_index in range(training_data.shape[2]):
+				sum =+ training_data[picture_index][y_index][x_index]
+		if sum <= 0:
+			picture_index += 2
+		nonzero_x_array = []
+		inner_array = [] #[x_min, x_center_left, x_center_right]
+		for y_index in range(training_data.shape[1]):
+			for x_index in range(training_data.shape[2]):
+				if training_data[picture_index][y_index][x_index] > 0:
+					print(picture_index)
+					nonzero_x_array.append(x_index)
+
+
+		left_of_center = []
+		right_of_center = []
+		for elem in nonzero_x_array:
+			if elem <= 13:
+				left_of_center.append(elem)
+			else:
+				right_of_center.append(elem)
+
+		inner_array.append(min(nonzero_x_array))
+		inner_array.append(max(left_of_center))
+		inner_array.append(min(right_of_center))
+		inner_array.append(max(nonzero_x_array))
+
+		outer_array.append(inner_array)
+		picture_index += 2
+
+
+
+
+
+	new_arr = np.array(outer_array)
+	print(new_arr)
+	print(new_arr.shape)
+
+
+
 
 
 
@@ -29,4 +75,9 @@ def main():
 	test_mask = np.isin(Y_test, numbers_used)
 	X_train, Y_train = X_train[train_mask], Y_train[train_mask]
 	X_test, Y_test = X_test[test_mask], Y_test[test_mask]
+	x_values(X_train)
 
+
+
+if __name__ == "__main__":
+	main()
